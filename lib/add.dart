67cd5d516fe45d12168dd8a1c9d1eedfed04543b
Juan Alberto Padilla Balbi <JuPadilla@edenorte.com.do>
2021-06-25 11:49:35 -0400

    Clase agregar +

--- lib/add.dart
+++ lib/add.dart
@@ -29,6 +29,66 @@ class _AddState extends State<Add> {
               icon: Icon(Icons.delete))
         ],
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Country Name"),
+                controller: nameController,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                style: TextStyle(color: Colors.white),
+                decoration: inputDecoration("Country Code"),
+                controller: codeController,
+              )
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        color: Colors.transparent,
+        child: BottomAppBar(
+          color: Colors.transparent,
+          child: RaisedButton(
+            color: Colors.black,
+            onPressed: () {
+              widget.db.create(nameController.text, codeController.text);
+              Navigator.pop(context);
+            },
+            child: Text(
+              "Save",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration(String labelText) {
+    return InputDecoration(
+      focusColor: Colors.white,
+      labelStyle: TextStyle(color: Colors.white),
+      labelText: labelText,
+      fillColor: Colors.white,
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(color: Colors.white),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(25.0),
+        borderSide: BorderSide(
+          color: Colors.black,
+          width: 2.0,
+        ),
+      ),
     );
   }
 }
